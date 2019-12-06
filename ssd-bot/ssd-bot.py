16728378e8a29bb93c5efeeb6d1636a2b10acf4f
Alex Xu <aleggsxu@gmail.com>
2019-12-05 21:40:26 -0800

    Bot now keeps track of posts commented on.

--- ssd-bot/ssd-bot.py
+++ ssd-bot/ssd-bot.py
@@ -2,7 +2,7 @@ import praw
 import os
 
 r = praw.Reddit('ssd-bot')
-bapcs = r.subreddit("buildapcsales")
+bapcs = r.subreddit("botlaunchpad")
 
 if not os.path.isfile("commented_on.txt"):
     commented_on = []
@@ -12,20 +12,11 @@ else:
         commented_on = commented_on.split("\n")
         commented_on = list(filter(None, commented_on))
 
-if not os.path.isfile("ssd_posts.txt"):
-    ssd_posts = []
-else:
-    with open("ssd_posts.txt", "r") as f:
-        ssd_posts = f.read()
-        ssd_posts = ssd_posts.split("\n")
-        ssd_posts = list(filter(None, ssd_posts))
-
-for submission in bapcs.new(limit = 200):
+for submission in bapcs.new(limit = 20):
     if submission.id not in commented_on and "[SSD]" in submission.title:
+        submission.reply("Toshiba Inland Samsung Crucial Intel E12p 660 Premium P1 EVO++")
         commented_on.append(submission.id)
-        ssd_posts.append(submission.title)
 
-
-
-for post in ssd_posts:
-    print(post)
+with open("commented_on.txt", "w") as f:
+    for submission_id in commented_on:
+        f.write(submission_id + "\n")
