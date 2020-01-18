76e923490336127663e4ab6a9b73e2a72ba9f205
Alex <aleggsxu@gmail.com>
2020-01-17 22:33:16 -0800

    Cleaned up some formatting.

--- ssd_bot/ssd_bot.py
+++ ssd_bot/ssd_bot.py
@@ -17,17 +17,23 @@ else:
 for submission in bapcs.new(limit=10):
     # should be set based on avg posts per time interval
     if submission.id not in commented_on and "[SSD]" in submission.title:
-        comment = ""
+        sentence_comment = ""
         commented_on.append(submission.id)
         title = submission.title
         brands, models = main()
         brand, model = title_filter(title, brands, models)
-        comment += f"This SSD seems to be a {brand} {model}."
+        sentence_comment += f"This SSD seems to be a {brand} {model}."
         
         controller, dram, nandtype, category = lookup(brand, model)
-        comment += f" It is {nandtype}, with a {controller} controller. {dram} for DRAM."
-        comment += f" u/NewMaxx classifies this as {category}."
-        submission.reply(comment)
+        sentence_comment += f" It is {nandtype}, with a {controller} controller. {dram} for DRAM."
+        sentence_comment += f" u/NewMaxx classifies this as {category}."
+        # submission.reply(sentence_comment)
+
+        info_comment = f"Serving up some information about this {brand} {model}.\n\n"
+        info_comment += f"NAND type: **{nandtype}**\n\nDRAM: **{dram}**\n\nController: **{controller}**\n\n"
+        info_comment += f"Classified as: **{category}**\n\n"
+        # info_comment += "This bot made possible by NewMaxx and company."
+        submission.reply(info_comment)
 
 with open("commented_on.txt", "w") as f:
     for submission_id in commented_on:
